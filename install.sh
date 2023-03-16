c7c20a4bc0d47b53fa2c5c8106350e3c08e7e439
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2023-03-16 19:54:21 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@ apt update
 apt install git tsu -y
 cd /data
 sudo rm -rf VPN
-sudo git clone -b VPN https://github.com/Qiu2zhi1zhe3/VPN
+sudo git clone -b VPN https://github.com/DauDau432/VPN
 sudo chmod -R 755 VPN
 sudo mkdir -p /data/adb/modules/VPN/system/bin/
 sudo ln -sf /data/VPN/start.sh /data/adb/modules/VPN/system/bin/vpn
-su -c 'cat << EOF >> /data/adb/modules/VPN/service.sh
+sudo -c 'cat << EOF >> /data/adb/modules/VPN/service.sh
 #!/system/bin/sh
 until [[ \$(getprop sys.boot_completed) -eq 1 ]] ; do
 sleep 3
@@ -18,8 +18,8 @@ VPN="/data/VPN"
 EOF'
 echo && read -p "  Vui lòng nhập link subscribe: " link
 if [[ $link != "" ]] ;  then
-su -c sed -i "s+link\ sub+$link+g" /data/VPN/config.yaml
-su -c sed -i "s+link\ sub+$link+g" /data/VPN/config.json
+sudo -c sed -i "s+link\ sub+$link+g" /data/VPN/config.yaml
+sudo -c sed -i "s+link\ sub+$link+g" /data/VPN/config.json
 sudo curl -sL $link -o /data/VPN/run/sub.txt
 fi
-echo ' Hãy gõ lệnh "su" và "vpn" để truy cập vào menu sau khi khởi động lại '
+echo ' Hãy gõ lệnh "sudo" và "vpn" để truy cập vào menu sau khi khởi động lại '
